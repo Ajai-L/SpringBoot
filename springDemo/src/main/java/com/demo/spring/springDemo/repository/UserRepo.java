170ddd462866de203a8618b47a774b3ca20eba6e
Ajai <ajailight7646337@gmail.com>
2025-10-16 10:33:04 +0530

    Delete springDemo directory

--- springDemo/src/main/java/com/demo/spring/springDemo/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demo.spring.springDemo.repository;
-
-import com.demo.spring.springDemo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
